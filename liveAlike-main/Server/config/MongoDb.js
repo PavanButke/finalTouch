bb4565c9f43f857a804ee6ec7a48da3f257f6675
PavanButke <butkepavan55@gmail.com>
2022-09-07 17:16:00 +0530

    resolved bugs

---
+++ liveAlike-main/Server/config/MongoDb.js
@@ -0,0 +1,23 @@
+const mongoose =  require("mongoose");
+
+const connectDatabase = async()=>{
+    
+    try{
+        const connection = await mongoose.connect(process.env.MONGO_URL,{
+            useUnifiedTopology: true,
+            useNewUrlParser:true,
+
+        });
+
+        console.log("Connected To MongoDb");
+
+
+    }catch(error){
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+
+};
+
+module.exports = connectDatabase;
+// export default connectDatabase;
